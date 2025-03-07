d7a5f3a784249a312a61c1d90b9e22c1ad723539
zjylovelxh <128208533+zjylovelxh@users.noreply.github.com>
2025-03-08 00:40:38 +0800

    2025.3.8

---
+++ src/main/java/com/zjy/mianshist/controller/PostThumbController.java
@@ -0,0 +1,56 @@
+package com.zjy.mianshist.controller;
+
+import com.zjy.mianshist.common.BaseResponse;
+import com.zjy.mianshist.common.ErrorCode;
+import com.zjy.mianshist.common.ResultUtils;
+import com.zjy.mianshist.exception.BusinessException;
+import com.zjy.mianshist.model.dto.postthumb.PostThumbAddRequest;
+import com.zjy.mianshist.model.entity.User;
+import com.zjy.mianshist.service.PostThumbService;
+import com.zjy.mianshist.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+                                         HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
---
+++ src/main/java/com/zjy/mianshist/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.zjy.mianshist.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zjy.mianshist.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
